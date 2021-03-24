a3ae4418f973bb5531d94881cfc70e57d5e17050
Ognjen Marinkovic <pingvincar@gmail.com>
2021-03-24 12:21:27 +0100

    Fixed API

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,18 @@ import TopBar from '../components/topbar/topbar'
 
 import { Box } from '@chakra-ui/react'
 import NewestVideo from '../components/newest-video/newest-video'
+import OtherVideos from '../components/other-videos/other-videos'
 
-const CHANNEL_ID = "UC94DrDp_sU68e2nol9-sJJg";
-const YOUTUBE_API="https://www.googleapis.com/youtube/v3/search"
+const PLAYLIST_ID = "UU94DrDp_sU68e2nol9-sJJg";
+const YOUTUBE_API="https://youtube.googleapis.com/youtube/v3/playlistItems"
 
 export async function getStaticProps() {
-  const res = await fetch(`${YOUTUBE_API}?key=${process.env.YOUTUBE_API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=4`)
-  const data = await res.json()
-  const videos = await data.items
+  const res = await fetch(`${YOUTUBE_API}?part=snippet%2CcontentDetails&maxResults=4&playlistId=${PLAYLIST_ID}&key=${process.env.YOUTUBE_API_KEY}`)
+  console.log(res)
+  const data = await res.json
+  const videos = await data.items ? data.items : []
+  
+  console.log(`${YOUTUBE_API}?key=${process.env.YOUTUBE_API_KEY}&part=snippet%2CcontentDetails&maxResults=50&playlistId=${PLAYLIST_ID}`)
   
   return {
     props: {
@@ -22,11 +26,16 @@ export async function getStaticProps() {
 }
 
 export default function Home({ videos }) {
+
+  const newestVideo = videos.shift()
+  const otherVideos = videos
+
   return (
     <> 
       <TopBar />
       <Box display="flex" flexDirection="column" alignItems="center">
-        <NewestVideo video={videos[0]} />
+        <NewestVideo video={newestVideo} />
+        <OtherVideos videos={otherVideos} />
       </Box>
     </>
   )
