62ef09f1ead9f7d71e52af15f7b74d9c9cb0c7de
Ognjen Marinkovic <pingvincar@gmail.com>
2021-03-25 10:31:20 +0100

    Fixed responsiveness

--- components/other-videos/other-videos.js
+++ components/other-videos/other-videos.js
@@ -8,7 +8,7 @@ export default function OtherVideos ({ videos }) {
     <Box as="section" w="100%" bg="linear-gradient(#AE72CE, #9C56C2)" p="8px" pb="16px">
         <Heading as="h3" color="white" mb="16px">Остале емисије</Heading>
         <Box display="flex" alignItems="center" h="90%">
-            <UnorderedList display="flex" justifyContent="space-around" w="100%" listStyleType="none" flexWrap="wrap"> 
+            <UnorderedList ml='0' display="flex" justifyContent="space-around" w="100%" listStyleType="none" flexWrap="wrap"> 
                 {videos.map((video) => (
                     <ListItem key={video.snippet.resourceId.videoId}>
                         <VideoCard key={video.snippet.resourceId.videoId} videoId={video.snippet.resourceId.videoId} title={video.snippet.title}/>
