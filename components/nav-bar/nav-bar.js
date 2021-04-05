9cf3f69cb0cf4a4e89b7abb4b34c2144ec0b5fa7
Ognjen Marinkovic <pingvincar@gmail.com>
2021-04-05 22:01:19 +0200

    Fixed nav bar size

--- components/nav-bar/nav-bar.js
+++ components/nav-bar/nav-bar.js
@@ -2,7 +2,7 @@ import { UnorderedList, ListItem, Link } from "@chakra-ui/layout";
 import NextLink from 'next/link'
 
 export default function NavBar() {
-    return <UnorderedList display='flex' w='10%' justifyContent='space-around' listStyleType='none' fontWeight='700' color='white' alignItems='center'>
+    return <UnorderedList display='flex' w='200px' justifyContent='space-around' listStyleType='none' fontWeight='700' color='white' alignItems='center'>
         <ListItem><NextLink href='/' passHref><Link>Почетна</Link></NextLink></ListItem>
         <ListItem><NextLink href='/articles' passHref><Link>Чланци</Link></NextLink></ListItem>
     </UnorderedList>
